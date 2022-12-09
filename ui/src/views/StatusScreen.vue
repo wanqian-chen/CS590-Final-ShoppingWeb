23b5fbd78e68bd1b4896aeb2bdcdd23c2720c382
mx.ohe <1299686758@qq.com>
2022-12-08 19:36:17 -0500

    update display of orderItem in allOrders

--- ui/src/views/StatusScreen.vue
+++ ui/src/views/StatusScreen.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="mx-3 my-3">
     <b-button @click="refresh" class="mb-2">Refresh</b-button>
-    <b-table :items="orders" />
+    <!-- <b-table :items="orders" /> -->
+    <b-table :items="orders" :fields="fields">
+      <template #cell(order.value)="cellScope">
+        <div v-for="(ingredients, item) in cellScope.value">
+          {{item}} with 
+          <span v-for="ingredient in ingredients">
+            {{ingredient}},
+          </span>
+        </div>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, Ref } from 'vue'
+import { Order } from "../../../server/data"
 
 const orders: Ref<Order[]> = ref([])
+
+const fields = [
+  "_id",
+  "customerId",
+  "state",
+  {
+    key: "order.value",
+    label: "Order Item"
+  },
+  "operatorId"
+]
 
 async function refresh() {
   orders.value = await (await fetch("/api/orders")).json()
