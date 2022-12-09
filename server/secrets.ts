1abaea3cac81ca55e8846dbe22eb49fe7e418e4b
mx.ohe <1299686758@qq.com>
2022-12-09 18:51:18 -0500

    delete secret

--- server/secrets.ts
+++ server/secrets.ts
@@ -1,6 +1,6 @@
 export const keycloak = {
   client_id: "smoothie",
-  client_secret: "zXgEm0tfnIPtJ0CE2E0hvNBxevHyYENz", // TODO
+  client_secret: "", // TODO
   redirect_uris: ["http://127.0.0.1:8080/api/login-callback"],
   post_logout_redirect_uris: [""],
   response_types: ["code"],
